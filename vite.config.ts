95fb044b80097058ca4c333727a4d21a85a5d4b5
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2025-07-09 15:19:58 +0000

    Update dev server config to support devcontainer
    Serving with HTTPS is not necessary for devcontainer, as it is port-forwarded over HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,46 @@
 import { readFileSync } from "node:fs";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type ServerOptions } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 import "dotenv/config";
 
-function requiredEnvVar(name: string) {
-  const value = process.env[name];
-  if (typeof value !== "string") {
-    throw new Error(`Missing required env var: ${name}`);
-  }
-  return value;
-}
+const vars = {
+  host: "HOST",
+  httpsEnabled: "HTTPS_ENABLED",
+  httpsKeyPath: "HTTPS_KEY_PATH",
+  httpsCertPath: "HTTPS_CERT_PATH",
+  spotifyClientId: "PUBLIC_SPOTIFY_CLIENT_ID",
+  spotifyClientSecret: "SPOTIFY_CLIENT_SECRET",
+  sessionSecret: "SESSION_SECRET",
+} as const;
+type EnvVar = (typeof vars)[keyof typeof vars];
 
-const keyPath = requiredEnvVar("HOST_KEY_PATH");
-const certPath = requiredEnvVar("HOST_CERT_PATH");
-const key = readFileSync(keyPath, { encoding: "utf8" });
-const cert = readFileSync(certPath, { encoding: "utf8" });
+warnIfEnvVarsMissing(
+  [vars.spotifyClientId, vars.spotifyClientSecret],
+  "Spotify integration will not work."
+);
+requiredEnvVar(
+  vars.sessionSecret,
+  "This is required for session cookies. Generate a secret with this command: openssl rand -base64 32"
+);
+
+let https: ServerOptions["https"] = undefined;
+if (booleanEnvVar(vars.httpsEnabled)) {
+  const keyPath = requiredEnvVar(vars.httpsKeyPath);
+  const certPath = requiredEnvVar(vars.httpsCertPath);
+  https = {
+    key: readFileSync(keyPath, { encoding: "utf8" }),
+    cert: readFileSync(certPath, { encoding: "utf8" }),
+  };
+}
 
 export default defineConfig({
   server: {
     host: "127.0.0.1",
     port: 3000,
-    https: { key, cert },
+    https,
     proxy: {},
   },
   envPrefix: "PUBLIC_",
@@ -33,3 +50,28 @@ export default defineConfig({
     solidPlugin(),
   ],
 });
+
+function warnIfEnvVarsMissing(varNames: EnvVar[], message: string) {
+  const missing = varNames.filter((name) => !process.env[name]);
+  if (missing.length === 0) {
+    return;
+  }
+  console.warn(`Missing env vars: ${missing.join(", ")}. ${message}`);
+}
+
+function requiredEnvVar(name: EnvVar, tip?: string) {
+  const value = process.env[name];
+  if (!value) {
+    let message = `Missing required env var: ${name}`;
+    if (tip) {
+      message += `. ${tip}`;
+    }
+    throw new Error(message);
+  }
+  return value;
+}
+
+function booleanEnvVar(name: EnvVar) {
+  const value = process.env[name]?.trim().toLowerCase();
+  return value != null && value !== "false" && value !== "0" && Boolean(value);
+}
