3a7064e9fbaaf598d1074daba50bcfe64ab67445
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2025-07-20 13:31:53 +0900

    Add range input that sets volume knob position

--- src/components/player/volume-control-panel/VolumeControlPanel.tsx
+++ src/components/player/volume-control-panel/VolumeControlPanel.tsx
@@ -1,10 +1,28 @@
 import styles from "./VolumeControlPanel.module.css";
 
 export function VolumeControlPanel() {
+  let knobElement!: HTMLDivElement;
+  function setKnobPosition(value: string) {
+    knobElement.style.left = `${value}%`;
+  }
+  function onInput(value: string) {
+    setKnobPosition(value);
+  }
+  function onChange(value: string) {
+    setKnobPosition(value);
+  }
   return (
     <div class={`panel ${styles.background}`}>
-      <div>
-        <button class={styles.knob} type="button" />
+      <div class={styles.slider}>
+        <div class={styles["knob-container"]}>
+          <div class={`${styles.knob}`} ref={knobElement} />
+        </div>
+        <input
+          type="range"
+          class={styles.range}
+          onInput={(e) => onInput(e.target.value)}
+          onChange={(e) => onChange(e.target.value)}
+        />
       </div>
     </div>
   );
