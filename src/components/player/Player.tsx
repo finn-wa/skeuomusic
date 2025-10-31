cc8ee2b4def33e88156e6eb10794f8940af7fb8f
Finn Welsford-Ackroyd <finnwa24@gmail.com>
2025-11-01 00:49:51 +1100

    Try out the web player sdk for in-browser playback

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,4 +1,6 @@
-import { createSignal } from "solid-js";
+import { createSignal, onMount } from "solid-js";
+import { initSpotifyPlayer } from "spotify-api-client";
+import { useAuthContext } from "~/lib/client/auth-context";
 import { usePlayerContext } from "~/lib/player/player-context";
 import { AlbumArt } from "../album-art/AlbumArt";
 import { PlaybackControlOverlay } from "./playback-control-overlay/PlaybackControlOverlay";
@@ -8,7 +10,34 @@ import { VolumeControlPanel } from "./volume-control-panel/VolumeControlPanel";
 
 export function Player() {
   const { state } = usePlayerContext();
+  const auth = useAuthContext();
   const [isOverlayShown, setOverlayShown] = createSignal(true);
+
+  onMount(async () => {
+    const SpotifyPlayer = initSpotifyPlayer(window);
+    const player = new SpotifyPlayer({
+      name: "skeuomusic",
+      getOAuthToken: (callback) => {
+        auth
+          .spotifyAuth()!
+          .getAccessToken()
+          .then((token) => {
+            callback(token!.access_token);
+          });
+      },
+      enableMediaSession: true,
+      volume: 1,
+    });
+    player.addListener("ready", ({ device_id }) => {
+      console.log("Ready with Device ID", device_id);
+    });
+
+    player.addListener("not_ready", ({ device_id }) => {
+      console.log("Device ID has gone offline", device_id);
+    });
+    const res = await player.connect();
+    console.log({ connected: res });
+  });
 
   return (
     <div class="player-container">
