7a11485a73b5de9254b1579a91b8ef68f12e9a83
Finn Welsford-Ackroyd <finnw_dev@sent.com>
2026-05-19 00:27:11 +1000

    Get app running again

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -1,8 +1,10 @@
+import { PluginOption } from "vite";
 import solid from "vite-plugin-solid";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [solid()],
+  // Vite version mismatch in solidJS plugin
+  plugins: [solid() as PluginOption],
   resolve: {
     conditions: ["development", "browser"],
   },
