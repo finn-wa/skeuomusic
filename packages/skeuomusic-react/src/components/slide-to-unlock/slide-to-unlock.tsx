243b2d962c0857e2b5a8bf85dbdca645471a6d0a
Finn Welsford-Ackroyd <finnw_dev@sent.com>
2026-05-30 16:53:13 +1000

    Clean up slide-to-unlock code

--- packages/skeuomusic-react/src/components/slide-to-unlock/slide-to-unlock.tsx
+++ packages/skeuomusic-react/src/components/slide-to-unlock/slide-to-unlock.tsx
@@ -11,11 +11,18 @@ export interface SlideToUnlockProps {
 
 /** iOS 6 lock-screen-style slide to unlock component */
 export default function SlideToUnlock({ onUnlock, text = "slide to unlock" }: SlideToUnlockProps) {
+  // The arrow image that the user drags along the track
   const thumbRef = useRef<HTMLImageElement>(null);
+  // The slider track that contains the arrow
   const trackRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLSpanElement>(null);
-  const bounds = useRef({ left: 0, right: 100 });
+  // The text on the track
+  const textRef = useRef<HTMLButtonElement>(null);
+  // The limits of the thumb's X position on the page so that it stays within the track
+  const boundsRef = useRef<{ left: number; right: number }>(null);
+  // The horizontal difference in pixels between the left of the thumb and the place where the pointer pressed
+  const pointerOffset = useRef(0);
 
+  /** Plays an unlock sound and calls the onUnlock callback prop. */
   function unlock() {
     new Audio(unlockSound).play().catch((error) => {
       console.error("Failed to play unlock audio", error);
@@ -23,31 +30,76 @@ export default function SlideToUnlock({ onUnlock, text = "slide to unlock" }: Sl
     onUnlock();
   }
 
+  /** Triggers an unlock, sliding the thumb to the end of the track. */
+  function animatedUnlock() {
+    const progress = getProgress();
+    // Move thumb to end with transition proportional to how far it has to travel
+    const transitionTime = Math.round((1 - progress) * 150);
+    setThumbPosition(getBounds().right, `${transitionTime}ms ease-in`);
+
+    setTextAnimation("static");
+    setTextOpacity(0, true);
+    unlock();
+  }
+
   /** Returns slide progress as a decimal between 0 and 1 */
   function getProgress(): number {
     const currentTranslate = parseFloat(thumbRef.current!.style.translate || "0");
-    const maxTranslate = bounds.current.right - bounds.current.left;
+    const bounds = getBounds();
+    const maxTranslate = bounds.right - bounds.left;
     return currentTranslate / maxTranslate;
+  }
+
+  /** Computes bounds if they are uninitialized, then returns the current value. */
+  function getBounds(): { left: number; right: number } {
+    if (boundsRef.current === null) {
+      computeBounds();
+    }
+    return boundsRef.current!;
+  }
+
+  /**
+   * Computes the bounds for the thumb's X position in the page. Updates boundsRef with the result.
+   */
+  function computeBounds() {
+    const trackRect = trackRef.current!.getBoundingClientRect();
+    const thumbRect = thumbRef.current!.getBoundingClientRect();
+    boundsRef.current = {
+      left: trackRect.left,
+      right: trackRect.right - thumbRect.width,
+    };
+  }
+
+  /**
+   * Updates the thumb's position along the slider.
+   *
+   * @param position the x position on the page in pixels
+   * @param transition a transition specifier for translate (e.g. "400ms"), or null to disable transitions
+   */
+  function setThumbPosition(position: number, transition: string | null) {
+    const bounds = getBounds();
+    const boundedX = Math.max(Math.min(position, bounds.right), bounds.left);
+    const transformX = boundedX - bounds.left;
+    const thumb = thumbRef.current!;
+    thumb.style.translate = `${transformX}px`;
+    thumb.style.transition = transition === null ? "none" : `translate ${transition}`;
+  }
+
+  function setTextAnimation(style: "animated" | "static"): void {
+    textRef.current!.className = `${styles.text} ${styles[style]}`;
+  }
+
+  function setTextOpacity(opacity: number, useTransition: boolean) {
+    textRef.current!.style.transition = useTransition ? "opacity 50ms" : "none";
+    textRef.current!.style.opacity = String(opacity);
   }
 
   function onPointerDown(event: PointerEvent<HTMLImageElement>): void {
     // Remove text shine while dragging
-    textRef.current!.classList = `${styles.text} ${styles.static}`;
-
-    // |---|                    trackLeft
-    // |-----------|            thumbLeft
-    // |--------------|         event.clientX
-    //             |--|         thumbOffset
-    //     [=======(thumb)===]
-
-    const trackRect = trackRef.current!.getBoundingClientRect();
-    const thumbRect = thumbRef.current!.getBoundingClientRect();
-    const thumbOffset = event.clientX - thumbRect.left;
-    bounds.current = {
-      left: trackRect.left + thumbOffset,
-      right: trackRect.right - (thumbRect.width - thumbOffset),
-    };
-    thumbRef.current!.style.transition = "none";
+    setTextAnimation("static");
+    // Re-compute bounds in case the window size has changed
+    computeBounds();
+    pointerOffset.current = event.clientX - thumbRef.current!.getBoundingClientRect().left;
     // keep drag alive outside element
     thumbRef.current!.setPointerCapture(event.pointerId);
   }
@@ -56,10 +108,8 @@ export default function SlideToUnlock({ onUnlock, text = "slide to unlock" }: Sl
     if (event.buttons === 0) {
       return;
     }
-    const boundedX = Math.max(Math.min(event.clientX, bounds.current.right), bounds.current.left);
-    const transformX = boundedX - bounds.current.left;
-    thumbRef.current!.style.translate = `${transformX}px`;
-    textRef.current!.style.opacity = String(Math.max(0, 0.75 - getProgress()));
+    setThumbPosition(event.clientX - pointerOffset.current, null);
+    setTextOpacity(Math.max(0, 0.75 - getProgress()), false);
   }
 
   function onPointerUp(): void {
@@ -69,21 +119,24 @@ export default function SlideToUnlock({ onUnlock, text = "slide to unlock" }: Sl
       return;
     }
     // Resume showing the text animation
-    textRef.current!.classList = `${styles.text} ${styles.animated}`;
-    textRef.current!.style.opacity = "1";
+    setTextAnimation("animated");
+    setTextOpacity(1, true);
     // Return thumb to start with transition proportional to how far it has to travel
     const transitionTime = Math.round(progress * 200);
-    thumbRef.current!.style.transition = `translate ${transitionTime}ms ease-in`;
-    thumbRef.current!.style.translate = "0";
+    setThumbPosition(0, `${transitionTime}ms ease-in`);
   }
 
   return (
     <div className={styles.slider}>
       <div className={styles.wh100}>
         <div ref={trackRef} className={`${styles.track} ${styles.wh100}`}>
-          <span ref={textRef} className={`${styles.text} ${styles.animated}`}>
+          <button
+            ref={textRef}
+            className={`${styles.text} ${styles.animated}`}
+            onClick={animatedUnlock}
+          >
             {text}
-          </span>
+          </button>
         </div>
         <div className={styles.bounds} draggable={false}>
           <img
